8a6847159b1e4ad690b4cac92bd7ae2aaabf2856
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-08-01 15:37:03 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,9 @@
+<?php
+
+//Hallo Guys Scrip Ini Suport PTC juga Ya jadi Kalian Gk Ush Capek² Nyelesain PTC
+
+
+$ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64;Linux; Android 7.1.1; CPH1801) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36";
+$UserId = "EC-UserId-26314";
+
+
